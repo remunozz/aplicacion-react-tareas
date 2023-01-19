baf54075fd19de8812e1c5ad6996a2be36669ff7
remunozz <87038583+remunozz@users.noreply.github.com>
2023-01-19 18:59:03 -0500

    Agregar un formulario

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Logo from './componentes/Logo';
 import Tarea from './componentes/Tarea';
+import TareaFormulario from './componentes/TareaFormulario';
 
 function App() {
   return (
@@ -8,10 +9,12 @@ function App() {
       <Logo />
       <div className='tareas-lista-principal'>
         <h1>Mis Tareas</h1>
-        <Tarea texto='Aprender React'/>
+        <TareaFormulario />        
       </div>
     </div>
   );
 }
 
 export default App;
+
+// <Tarea texto='Aprender React'/>

117a7ce1e1db380bb68b956d61c194a374b04cda
remunozz <87038583+remunozz@users.noreply.github.com>
2023-01-18 22:09:58 -0500

    props completada  y estilo para tarea completada

--- src/componentes/Tarea.js
+++ src/componentes/Tarea.js
@@ -2,9 +2,9 @@ import React from 'react';
 import '../hojas-de-estilo/Tarea.css';
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
-function Tarea({texto}) {
+function Tarea({ texto, completada }) {
   return(
-    <div className='Tarea-contenedor'>
+    <div className={ completada ? 'tarea-contenedor completada':'tarea-contenedor'}>
       <div className='Tarea-texto'>
         {texto} 
       </div>

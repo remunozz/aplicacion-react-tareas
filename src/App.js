52a4302a7e230f266e5453b67a98ff4d2bb23152
remunozz <87038583+remunozz@users.noreply.github.com>
2023-01-18 20:52:26 -0500

    Lista de tareas

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="aplicacion-tareas">
       <Logo />
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+      </div>
     </div>
   );
 }
